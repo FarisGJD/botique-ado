1a070f92dcc3dad30e945966ab490d5bb6a479ff
Faris Dhoot <farisgjd@outlook.com>
2022-07-21 22:31:04 +0000

    Add stripe_element.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LO7TFFYCwMBWuXdta0RKGGms7eAuns2U5d3arenKqlBZsRvk4CW9UubJec1anLHRlNGOLMWjetZQ7jrL4TkCEPh00yV5Sl379',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
